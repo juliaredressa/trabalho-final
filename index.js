b5a503d5ffc0350b70d7df6a91a035a8946acfc0
Julia Redressa <julialimaredressa@gmail.com>
2023-12-13 21:40:25 -0300

    .

--- index.js
+++ index.js
@@ -417,6 +417,50 @@ app.get('/cadastro-de-usuarios.html', (req, res) => {
     <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #ffffff;
+        margin: 0;
+        padding: 0;
+    }
+    div {
+        width: 300px;
+        margin: 0 auto;
+        padding: 20px;
+        background-color: #de3163;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+    h1 {
+        text-align: center;
+        color: #ffffff;
+    }
+    label {
+        color: #ffffff;
+    }
+    input[type="text"],
+    input[type="date"],
+    button {
+        width: 100%;
+        padding: 8px;
+        margin-bottom: 10px;
+        border: none;
+        border-radius: 4px;
+    }
+    input[type="text"],
+    input[type="date"] {
+        background-color: #ffffff;
+    }
+    button {
+        background-color: #ffffff;
+        color: #de3163;
+        cursor: pointer;
+    }
+    button:hover {
+        background-color: #ff9999;
+    }
+    </style>
     <title>Cadastro de Usuários</title>
     </head>
     <body>
